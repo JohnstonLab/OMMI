072d0f4eb87a429e1dabbac57c405dd4692f838a
louisvdp <louis.vande.perre@ulb.ac.be>
2019-09-03 13:42:42 +0100

    Blinking LED via arduino done

--- ArduinoComm.py
+++ ArduinoComm.py
@@ -8,3 +8,28 @@ Script containing all fct in relation with arduino communication.
 
 """
 
+import serial
+import time
+from serial.tools.list_ports import comports
+#The function (comports())returns an iterable that yields tuples of three strings:
+
+    ## port name as it can be passed to serial.Serial or serial.serial_for_url()
+    ## description in human readable form
+    ## sort of hardware ID. E.g. may contain VID:PID of USB-serial adapters.
+
+portsList = comports()
+for i in range(0,len(portsList)):
+    try:
+        print "Trying...",portsList[i][0]
+        ser = serial.Serial(portsList[i][0], 9600)
+        break
+    except:
+        print "Failed to connect on",portsList[i][0]
+        i+=1
+
+
+for i in range(10):
+    time.sleep(0.5)
+    ser.write(b'H')   # send the pyte string 'H'
+    time.sleep(0.5)   # wait 0.5 seconds
+    ser.write(b'L')   # send the byte string 'L'
---
+++ Cyclops/redComm/redComm.ino
@@ -0,0 +1,73 @@
+#include <Cyclops.h>
+
+//dual LED triggering
+
+int timeDelay = 5 ; //ms
+int incomingByte;      // a variable to read incoming serial data into
+
+// Create a single cyclops object. CH0 corresponds to a physical board with
+// jumper pads soldered so that OC0, CS0, TRIG0, and A0 are used.
+// And limit current 1000 mA
+Cyclops cyclops0(CH0, 1000);
+                              
+bool rise = false; 
+bool fall = true;
+int frameCounter =0; 
+
+int voltage = 4095; //5V
+
+
+void triggerEventRising()
+{
+  rise=!rise;
+  //if (rise && fall) //green
+  //if (!rise && !fall) //red
+  if((frameCounter%5)==0)
+  {
+    cyclops0.dac_load_voltage(voltage); //Turn green LED ON
+    delay(timeDelay);
+    cyclops0.dac_load_voltage(0); //Turn green LED OF
+  }
+  //When cyclops detect a falling edge, call triggerEventFalling() method
+  cyclops0.set_trigger( triggerEventFalling, FALLING);
+  frameCounter+=1;
+}
+
+void triggerEventFalling()
+{
+  fall=!fall;
+  cyclops0.set_trigger( triggerEventRising, RISING);
+  
+}
+
+void setup()
+{
+  Cyclops::begin();
+  cyclops0.dac_load_voltage(0);
+  cyclops0.set_trigger( triggerEventRising, RISING);
+  //cyclops0.set_trigger( triggerEventFalling, FALLING);
+
+  // initialize serial communication:
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+    // Nothing to do, all action in the interrupt handler
+
+    if (Serial.available() > 0) {
+      // read the oldest byte in the serial buffer:
+      incomingByte = Serial.read();
+      // if it's a capital H (ASCII 72), turn on the LED:
+      if (incomingByte == 'H') {
+        cyclops0.dac_load_voltage(voltage);
+      }
+      // if it's an L (ASCII 76) turn off the LED:
+      if (incomingByte == 'L') {
+        cyclops0.dac_load_voltage(0);
+      }
+    }
+}
+
+
